b94a0a50b782c33b98144298a997dd31c6259e6c
JackFox77 <jacobfox000@googlemail.com>
2022-08-31 12:10:49 -0600

    init 3

--- LeaderBoard/server/services/MatchService.js
+++ LeaderBoard/server/services/MatchService.js
@@ -5,10 +5,18 @@ class MatchService {
 
 
     async createMatch(newMatch) {
+
+        const game = await dbContext.Game.findById(newMatch.gameId)
+
         const match = await dbContext.Match.create(newMatch)
         await match.populate('player', 'name picture')
-
+        await match.populate('game')
+        // @ts-ignore
+        await game.save()
+        return match
     }
+
+    async getMatches
 }
 
 
