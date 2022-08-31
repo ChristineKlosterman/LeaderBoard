3af88e240bfb1f7b4ae4905f7716d67e612b6fe8
Garrett Adams <92003987+garrett-adamss@users.noreply.github.com>
2022-08-31 11:52:36 -0600

    game

---
+++ LeaderBoard/server/models/Game.js
@@ -0,0 +1,23 @@
+import { Schema } from "mongoose";
+const ObjectId = Schema.Types.ObjectId
+
+
+export const GameSchema = new Schema ({
+name:{type: String, required: true},
+description:{type: String, required: true, maxlength: 200},
+rules:{type: String, required: true, maxlength: 300},
+img:{type: String, required: true},
+coverImg:{type: String, required: true},
+minPlayer:{type: Number, required: true},
+maxPlayer:{type: Number, required: true},
+type:{type: String, enum :[
+    'card game', 'board game', 'video game', 'sports'
+], required: true},
+})
+
+GameSchema.virtual('creator',{
+    localField: 'creatorId',
+    foreignField: '_id',
+    ref: 'Account',
+    justOne: true 
+})
