b723ba4dbd6f7d4290be7ed22f88f46dbc1e50d8
Casey Collins <collinscasey.homes@gmail.com>
2022-08-31 11:19:53 -0600

    commit

--- LeaderBoard.client/src/env.js
+++ LeaderBoard.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-98cu5tnf.us.auth0.com'
+export const clientId = 'WKOYTNBul4ufdkggNaIiz0Ykh2cVxGJD'
+export const audience = 'https://caseycollins.com'
